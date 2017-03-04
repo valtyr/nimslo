44c9c7b670ea15a97b3c1f76b8222b100a16a488
Valtýr Kjartansson <valtyr@valtyr.local>
2017-03-04 16:44:13 +0000

    Initial Commit

---
+++ egill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  egill
+//
+//  Created by Valtýr Örn Kjartansson on 3/4/17.
+//  Copyright © 2017 Valtýr Örn Kjartansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
